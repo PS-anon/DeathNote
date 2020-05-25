56dd25fb27d36435ccaa693672e4022a1ae6c5a5
PS-anon <60316035+PS-anon@users.noreply.github.com>
2020-05-25 13:39:45 +0000

    Add files via upload

--- port_scanner.py
+++ port_scanner.py
@@ -1,25 +1,94 @@
-import threading
-import socket
+from socket import *
+import time, sys
+from datetime import datetime
 
-target = input("Target: ")
+host = ' '
+common_ports = [
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    53,
+    80,
+    110,
+    120,
+    123,
+    125,
+    135,
+    143,
+    161,
+    194,
+    443
+    ]
 
-def portscan(port):
 
+
+try :
+  host = input("[+]Target ->")
+  print("[*]Target ", host)
+except KeyboardInterrupt :
+  print("[!]Well, bye !")
+  time.sleep(1)
+  sys.exit()
+
+hip = gethostbyname(host)
+
+print("""
+    You have 3 options :
+    1)Start a normal scan
+    2)Badass intense scan
+    3)Quick scan
+    Any other number will consider a quit option
+
+""")
+
+cho = int(input("-> "))
+def normal():
+  print("lol")
+if cho == 1 :
+  print("[+]Starting normal scan")
+  time.sleep(1)
+  normal()
+
+elif cho == 2 :
+  print("[+]Starting intense scan")
+  time.sleep(1)
+  intense()
+elif cho == 3 :
+  print("[+]Starting quick scan")
+  time.sleep(1)
+  quick()
+
+else :
+  print("See ya !")
+  sys.exit(1)
+
+def scan_port(host, port , r_port=1):
+  try :
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+    conn = s.connect_ex(host, port)
+    if conn == 0 :
+      r_code == conn
+    s.close()
+  except Exception as e :
+    pass
+  return r_code
 
+
+def normal(host):
+  for port in range(1, 1000):
     try:
-        con = s.connect((target,port))
+      response = scan_port(host, port)
+      if response == 0 :
+        print("[!] Port %d : Open" % (port))
+    except Exception as e :
+      pass
+def quick():
+  print("Still in dev")
 
-        print('Port :',port,"is open.")
+def intense():
+  print("Still in dev")
 
-        con.close()
-    except: 
-        pass
-r = 1 
-for x in range(1,100): 
 
-    t = threading.Thread(target=portscan,kwargs={'port':r}) 
-
-    r += 1     
-    t.start() 
